77eb22ab685485b42f657c85836de98edd92f591
Fernando Matsuo Santos <fernando.matsuo@moduscreate.com>
2021-10-19 22:16:31 -0300

    test

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,11 +14,15 @@ resource "azurerm_resource_group" "resource_group" {
   }
 }
 
+resource "azurerm_storage_account" "storage_account" {
+  name                     = "atlantis_test"
+  resource_group_name      = azurerm_resource_group.resource_group.name
+  location                 = azurerm_resource_group.resource_group.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
 
-
-
-
-
-
-
-
+  tags = {
+    creator    = local.tag_creator
+    env        = var.stage
+  }
+}
